052fa33b6f0f239169662a393f8d977f5c4235ff
Alejandro Rueda <alejandro.rueda@nazaries.com>
2025-03-31 09:13:36 +0200

    version.rb updated

--- lib/decidim/file_authorization_handler/version.rb
+++ lib/decidim/file_authorization_handler/version.rb
@@ -2,7 +2,7 @@
 
 module Decidim
   module FileAuthorizationHandler
-    DECIDIM_VERSION = "0.29.0"
+    DECIDIM_VERSION = "0.29.2"
 
     # Uses the latest matching Decidim version for
     # - major, minor and patch
